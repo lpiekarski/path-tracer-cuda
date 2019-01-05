e9407bc6a887a3d585f8e7ac955f3a6e78424846
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-05 03:06:51 +0100

    added device code, device_ctr

--- ray.h
+++ ray.h
@@ -6,34 +6,48 @@
 
 _RTC_BEGIN
     // CLASS TEMPLATE ray
-template <size_t N>
+template <size_t _Dims>
     class ray {
     private:
-        normal<N> direction;
-        vector<N> origin;
+        normal<_Dims> direction;
+        vector<_Dims> origin;
 
     public:
+        using _Mytype = ray<_Dims>;
+
         _DEVHOST ray() : direction(), origin() {}
 
-        _DEVHOST ray(const vector<N>& direction, const vector<N>& origin)
+        _DEVHOST ray(const vector<_Dims>& direction, const vector<_Dims>& origin)
             : direction(direction),
             origin(origin) {}
 
-        _DEVHOST ray(const vector<N>& direction)
+        _DEVHOST ray(const vector<_Dims>& direction)
             : direction(direction),
             origin() {}
 
-        _DEVHOST vector<N> get_point(const vec_type& distance) const {
+        _DEVHOST vector<_Dims> get_point(const vec_type& distance) const {
             return origin + (distance * direction);
         }
 
-        _DEVHOST vector<N>& dir() { return direction; }
+        _DEVHOST vector<_Dims>& dir() { return direction; }
 
-        _DEVHOST vector<N> dir() const { return direction; }
+        _DEVHOST vector<_Dims> dir() const { return direction; }
 
-        _DEVHOST vector<N>& o() { return origin; }
+        _DEVHOST vector<_Dims>& o() { return origin; }
 
-        _DEVHOST vector<N> o() const { return origin; }
+        _DEVHOST vector<_Dims> o() const { return origin; }
+#ifdef RTC_USE_CUDA
+        template <typename... _Args>
+        //typename = enable_if_t<is_constructible<_Mytype, _Args...>::value>
+        _HOST static _Mytype* device_ctr(_Args... args) {
+            _Mytype h_ret(args...);
+            _Mytype *d_ret_ptr;
+            size_t sz = sizeof(_Mytype);
+            cudaMalloc((void**)&d_ret_ptr, sz);
+            cudaMemcpy(d_ret_ptr, &h_ret, sz, cudaMemcpyHostToDevice);
+            return d_ret_ptr;
+        }
+#endif /* RTC_USE_CUDA */
     };
 
     using ray3 = ray<3>;
