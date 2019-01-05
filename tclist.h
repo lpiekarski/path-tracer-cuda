9a73e77da5a0066b0f39672544cbb52a76c7900a
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-05 03:07:55 +0100

    added device code

--- tclist.h
+++ tclist.h
@@ -1,42 +1,38 @@
 #ifndef __TCLIST_H__
 #define __TCLIST_H__
 
-#include <memory>
-#include <tuple>
-#include <type_traits>
-#include <vector>
-
 #include "rtc.h"
 #include "tracable.h"
+#include "type_traits.h"
 
 _RTC_BEGIN
     // CLASS TEMPLATE tracable_list
-template <size_t N,
+template <size_t _Dims,
     size_t BPP>
     class tracable_list {
     private:
-        _vector<tracable<N, BPP>*> tracables;
+        _vector<tracable<_Dims, BPP>*> tracables;
 
     public:
-        tracable_list() : tracables() {}
+        _DEVHOST tracable_list() : tracables() {}
 
-        tracable_list(const tracable_list& other)
+        _DEVHOST tracable_list(const tracable_list& other)
             : tracables(other.tracables) {}
 
-        tracable_list& operator=(const tracable_list& other) {
+        _DEVHOST tracable_list& operator=(const tracable_list& other) {
             tracables = other.tracables;
         }
 
-        void add(tracable<N, BPP>& t) { tracables.push_back(&t); }
+        _DEVHOST void add(tracable<_Dims, BPP>& t) { tracables.push_back(&t); }
 
-        bool ray_intersection(const ray<N>& r,
-            tracable<N, BPP>** tr_obj,
+        _DEVHOST bool ray_intersection(const ray<_Dims>& r,
+            tracable<_Dims, BPP>** tr_obj,
             vec_type& intersection_dist) const {
             intersection_dist = MAX_RAY_LENGTH;
             vec_type dst;
             bool intersection = false;
             for (size_t i = 0; i < tracables.size(); ++i) {
-                tracable<N, BPP>* t = tracables[i];
+                tracable<_Dims, BPP>* t = tracables[i];
                 if (t->ray_intersection(r, dst) && dst < intersection_dist) {
                     *tr_obj = t;
                     intersection_dist = dst;
@@ -47,9 +43,9 @@ template <size_t N,
         }
     };
 
-template <size_t N,
+template <size_t _Dims,
     size_t BPP>
-    struct is_tracable_container<tracable_list<N, BPP>> 
+    struct is_tracable_container<tracable_list<_Dims, BPP>>
     : true_type {};
 _RTC_END
 
