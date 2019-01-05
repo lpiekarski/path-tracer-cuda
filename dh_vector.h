37a7829f1a4394b06869e0ff990b01fba6c71fa0
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-05 03:05:29 +0100

    added resize, push_back

--- dh_vector.h
+++ dh_vector.h
@@ -6,6 +6,7 @@ _RTC_BEGIN
 #define DH_VEC_SCALE_UP 2
 #define DH_VEC_SCALE_DOWN 4
 //TODO: make dh_vector thread safe
+//TODO: add device_ctr
 template <typename _Ty>
     class dh_vector {
     private:
@@ -32,14 +33,50 @@ template <typename _Ty>
             delete[] arr;
         }
 
-        _DEVHOST void push_back(const _Ty& _Elem) {
-            if (sz + 1 == arr_length) {
+        _DEVHOST void resize(size_t new_size) {
+            _Ty* new_arr = new _Ty[new_size];
+            size_t copy_length = new_size;
+            if (copy_length > sz)
+                copy_length = sz;
+            for (size_t i = 0; i < copy_length; ++i)
+                new_arr[i] = arr[i];
+            delete[] arr;
+            arr_length = new_size;
+            if (sz > arr_length)
+                sz = arr_length;
+            arr = new_arr;
+        }
 
+        //TODO: make thread safe
+        _DEVHOST void push_back(const _Ty& _Elem) {
+            if (sz == arr_length) {
+                _Ty* new_arr = new _Ty[arr_length * DH_VEC_SCALE_UP];
+                for (size_t i = 0; i < sz; ++i)
+                    new_arr[i] = arr[i];
+                delete[] arr;
+                arr_length *= DH_VEC_SCALE_UP;
+                arr = new_arr;
             }
             arr[sz++] = _Elem;
         }
 
-        _DEVHOST constexpr size_t begin() const noexcept {
+        //TODO: make thread safe
+        _DEVHOST void erase(size_t idx) {
+            _Ty* new_arr;
+            if ((sz - 1) * DH_VEC_SCALE_DOWN < arr_length)
+                arr_length /= DH_VEC_SCALE_UP;
+            new_arr = new _Ty[arr_length];
+            size_t j = 0;
+            for (size_t i = 0; i < sz; ++i) {
+                if (i == idx)
+                    continue;
+                new_arr[j++] = arr[i];
+            }
+            delete[] arr;
+            arr = new_arr;
+        }
+
+        _DEVHOST size_t begin() const noexcept {
             return 0;
         }
 
