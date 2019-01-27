5b7b5ffef692f23242fc2dbdf19c5156268db5a0
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-27 03:04:52 +0100

    changed throw_exc

--- exception.h
+++ exception.h
@@ -1,30 +1,35 @@
 #ifndef __EXCEPTION_H__
 #define __EXCEPTION_H__
+/*
+    throwing exceptions is currently 
+    not supported in device code.
+*/
 
 #include <exception>
 
 #include "rtc.h"
 
 _RTC_BEGIN
-class IndexOutOfBounds : public std::exception {
-public:
-    virtual char const * what() const noexcept {
-        return "IndexOutOfBounds";
-    }
-};
+    class IndexOutOfBounds : public std::exception {
+    public:
+        virtual char const * what() const noexcept {
+            return "IndexOutOfBounds";
+        }
+    };
 
-class ZeroNormalVector : public std::exception {
-public:
-    virtual char const * what() const noexcept {
-        return "ZeroNormalVector";
-    }
-};
+    class ZeroNormalVector : public std::exception {
+    public:
+        virtual char const * what() const noexcept {
+            return "ZeroNormalVector";
+        }
+    };
 
-#ifdef __CUDA_ARCH__
-#define throw_exc(E)
-#else /* !__CUDA_ARCH__ */
-#define throw_exc(E) throw E
-#endif /* __CUDA_ARCH__ */
+    _DEVHOST void throw_exc(const std::exception& e) {
+#ifndef __CUDA_ARCH__
+        throw e;
+#endif
+    }
+
 _RTC_END
 
 #endif /* __EXCEPTION_H__ */
