d8e117f13c8a6c24001f06784e1cdbc72fa0e207
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-27 03:04:26 +0100

    added device_ctr, added getters and setters

--- material.h
+++ material.h
@@ -1,7 +1,7 @@
 #ifndef __MATERIAL_H__
 #define __MATERIAL_H__
 
-#include <memory>
+#include <cmath>
 
 #include "rtc.h"
 #include "bitmap.h"
@@ -10,6 +10,7 @@ _RTC_BEGIN
     // CLASS TEMPLATE material
 template <size_t BPP>
     class material {
+        using _Mytype = material<BPP>;
     private:
         bitmap<BPP> *diffuse,
             *ambient,
@@ -17,6 +18,20 @@ template <size_t BPP>
             *refraction, 
             *normal;
             //subsurface
+
+        _DEVHOST color<BPP> get_from_bmp(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def,
+            bitmap<BPP>* bmp) {
+            if (bmp == nullptr)
+                return def;
+            x = x - floor(x);
+            y = y - floor(y);
+            size_t bmp_x = static_cast<size_t>(x * bmp->get_width());
+            size_t bmp_y = static_cast<size_t>(y * bmp->get_height());
+            return bmp->get(bmp_x, bmp_y);
+        }
+
     public:
         _DEVHOST material() 
             : diffuse(nullptr),
@@ -24,6 +39,73 @@ template <size_t BPP>
             reflection(nullptr),
             refraction(nullptr),
             normal(nullptr) {}
+
+        _DEVHOST material& set_diffuse(bitmap<BPP> *_Diffuse_bmp) {
+            diffuse = _Diffuse_bmp;
+            return *this;
+        }
+
+        _DEVHOST material& set_ambient(bitmap<BPP> *_Ambient_bmp) {
+            ambient = _Ambient_bmp;
+            return *this;
+        }
+
+        _DEVHOST material& set_reflection(bitmap<BPP> *_Reflection_bmp) {
+            reflection = _Reflection_bmp;
+            return *this;
+        }
+
+        _DEVHOST material& set_refraction(bitmap<BPP> *_Refraction_bmp) {
+            refraction = _Refraction_bmp;
+            return *this;
+        }
+
+        _DEVHOST material& set_normal(bitmap<BPP> *_Normal_bmp) {
+            normal = _Normal_bmp;
+            return *this;
+        }
+
+        _DEVHOST color<BPP> get_diffuse(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def) {
+            return get_from_bmp(x, y, def, diffuse);
+        }
+
+        _DEVHOST color<BPP> get_ambient(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def) {
+            return get_from_bmp(x, y, def, ambient);
+        }
+
+        _DEVHOST color<BPP> get_reflection(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def) {
+            return get_from_bmp(x, y, def, reflection);
+        }
+
+        _DEVHOST color<BPP> get_refraction(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def) {
+            return get_from_bmp(x, y, def, refraction);
+        }
+
+        _DEVHOST color<BPP> get_normal(const vec_type& x,
+            const vec_type& y,
+            const color<BPP>& def) {
+            return get_from_bmp(x, y, def, normal);
+        }
+#ifdef RTC_USE_CUDA
+        template <typename... _Args>
+        typename = enable_if_t<is_constructible<_Mytype, _Args...>::value>
+            _HOST static _Mytype* device_ctr(_Args... args) {
+            _Mytype h_ret(args...);
+            _Mytype *d_ret_ptr;
+            size_t sz = sizeof(_Mytype);
+            cudaMalloc((void**)&d_ret_ptr, sz);
+            cudaMemcpy(d_ret_ptr, &h_ret, sz, cudaMemcpyHostToDevice);
+            return d_ret_ptr;
+        }
+#endif /* RTC_USE_CUDA */
     };
 _RTC_END
 
