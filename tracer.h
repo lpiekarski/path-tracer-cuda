550a9f9ce3687e1c93f506ce802da4765c9fd83b
Łukasz Piekarski <wookashp98@gmail.com>
2019-01-27 03:03:44 +0100

    added __device__ and __host__ declspecs, added device_ctr

--- tracer.h
+++ tracer.h
@@ -45,75 +45,77 @@ namespace rtc {
     constexpr size_t MAX_RAY_BOUNCES = 5;
 
     template <class TracableContainer,
-        size_t N,
+        size_t _Dims,
         size_t BPP>
     class tracer {
     private:
         TracableContainer container;
-        camera<N> cam;
+        camera<_Dims> cam;
 
-        color<BPP> trace_ray_rec(const ray<N>& r, size_t recursion_level) const {
+        _DEVHOST color<BPP> trace_ray_rec(const ray<_Dims>& r, size_t recursion_level) const {
             if (recursion_level >= MAX_RAY_BOUNCES)
                 return color<BPP>();
-            tracable<N, BPP>* tr_obj = nullptr;
+            tracable<_Dims, BPP>* tr_obj = nullptr;
             vec_type dst;
             if (container.ray_intersection(r, &tr_obj, dst)) {
-                vector<N> intersection_point = r.get_point(dst);
+                vector<_Dims> intersection_point = r.get_point(dst);
                 color<BPP> ambient = tr_obj->ambient_color(r, intersection_point);
                 color<BPP> diffuse = tr_obj->diffuse_color(r, intersection_point);
-                ray<N> reflection_ray(tr_obj->get_reflection(r, intersection_point));
+                ray<_Dims> reflection_ray(tr_obj->get_reflection(r, intersection_point));
                 color<BPP> reflection = trace_ray_rec(reflection_ray, recursion_level + 1);
                 /*if (recursion_level >= 2) {
                     std::cout << recursion_level << std::endl;
                 }*/
-                return /*(reflection_ray.dir() + 1) / 2;*//*dot(r.dir(), vector<N>() - reflection_ray.dir()) * */reflection * diffuse + ambient;
+                return /*(reflection_ray.dir() + 1) / 2;*//*dot(r.dir(), vector<_Dims>() - reflection_ray.dir()) * */reflection * diffuse + ambient;
             }
             return color<BPP>();
         }
 
     public:
-        tracer() : container(), cam() {}
+        using _Mytype = tracer<TracableContainer, _Dims, BPP>;
 
-        tracer(const tracer&) = delete;
+        _DEVHOST tracer() : container(), cam() {}
 
-        tracer& operator=(const tracer&) = delete;
+        _DEVHOST tracer(const tracer&) = delete;
 
-        tracer(tracer&& other) : container(std::move(other.container)) {}
+        _DEVHOST tracer& operator=(const tracer&) = delete;
 
-        tracer& operator=(tracer&& other) {
+        _DEVHOST tracer(tracer&& other) : container(std::move(other.container)) {}
+
+        _DEVHOST tracer& operator=(tracer&& other) {
             container = std::move(other.container);
             return *this;
         }
 
-        void add_tracable(tracable<N, BPP>& t) {
+        _DEVHOST void add_tracable(tracable<_Dims, BPP>& t) {
             container.add(t);
         }
 
-    template <typename... Types, 
-        typename = enable_if_t<sizeof...(Types) == N - 1 && are_convertible<vec_type, Types...>::value>>
-        color<BPP> trace_from_camera(const Types&... screen_pos) const {
+    template <typename... _Args,
+        typename = enable_if_t<sizeof...(_Args) == _Dims - 1 && are_convertible<vec_type, _Args...>::value>>
+        _DEVHOST color<BPP> trace_from_camera(const _Args&... screen_pos) const {
             return trace_ray(cam.get_ray(screen_pos...));
         }
 
-        color<BPP> trace_from_camera(vec_type* screen_pos_arr) const {
+        _DEVHOST color<BPP> trace_from_camera(vec_type* screen_pos_arr) const {
             return trace_ray(cam.get_ray(screen_pos_arr));
         }
 
-        color<BPP> trace_ray(const ray<N>& r) const {
+        _DEVHOST color<BPP> trace_ray(const ray<_Dims>& r) const {
             return trace_ray_rec(r, 0);
         }
         
     template <size_t samples_per_pixel = 1,
-        typename... Types, 
-        typename = enable_if_t<N >= 3 && sizeof...(Types) == N - 1 && are_convertible<size_t, Types...>::value>>
-        _vector<bitmap<BPP>> draw_bitmap(Types... dims) {
+        typename... _Args,
+        typename = enable_if_t<_Dims >= 3 && sizeof...(_Args) == _Dims - 1 && are_convertible<size_t, _Args...>::value>>
+        _HOST _vector<bitmap<BPP>> draw_bitmap(_Args... dims) {
             std::random_device rd;
             std::mt19937 mt(rd());
             std::uniform_real_distribution<vec_type> dist(0, 1);
 
-            _array<size_t, N - 1> dims_arr = { static_cast<size_t>(dims)... };
+            _array<size_t, _Dims - 1> dims_arr = { static_cast<size_t>(dims)... };
             size_t ret_len = dims_arr[0] * dims_arr[1];
-            for (size_t i = 2; i < N - 1; ++i)
+            for (size_t i = 2; i < _Dims - 1; ++i)
                 ret_len *= dims_arr[i];
             _vector<bitmap<BPP>> ret;
             init_sample();
@@ -127,11 +129,11 @@ namespace rtc {
                     for (size_t y = 0; y < dims_arr[1]; ++y) {
                         color<BPP> sample_col;
                         for (size_t smp = 0; smp < samples_per_pixel; ++smp) {
-                            _array<vec_type, N - 1> screen_coord;
+                            _array<vec_type, _Dims - 1> screen_coord;
                             size_t c = i;
                             screen_coord[0] = (static_cast<vec_type>(x + random_vector[smp]) / dims_arr[0] - (vec_type)0.5) * (vec_type)2;
                             screen_coord[1] = (static_cast<vec_type>(y + random_vector[smp]) / dims_arr[1] - (vec_type)0.5) * (vec_type)2;
-                            for (size_t j = 2; j < N - 1; ++j) {
+                            for (size_t j = 2; j < _Dims - 1; ++j) {
                                 screen_coord[j] = (static_cast<vec_type>((c % dims_arr[j]) + random_vector[smp]) / dims_arr[j] - (vec_type)0.5) * (vec_type)2;
                                 c /= dims_arr[j];
                             }
@@ -147,6 +149,25 @@ namespace rtc {
             return ret;
         }
 
+#ifdef RTC_USE_CUDA
+    template <size_t samples_per_pixel = 1,
+        typename... _Args,
+        typename = enable_if_t<_Dims >= 3 && sizeof...(_Args) == _Dims - 1 && are_convertible<size_t, _Args...>::value>>
+        _DEVICE _vector<bitmap<BPP>> device_draw_bitmap(_Args... dims) {
+
+    }
+
+    template <typename... _Args>
+    //typename = enable_if_t<is_constructible<_Mytype, _Args...>::value>
+    _HOST static _Mytype* device_ctr(_Args... args) {
+        _Mytype h_ret(args...);
+        _Mytype *d_ret_ptr;
+        size_t sz = sizeof(_Mytype);
+        cudaMalloc((void**)&d_ret_ptr, sz);
+        cudaMemcpy(d_ret_ptr, &h_ret, sz, cudaMemcpyHostToDevice);
+        return d_ret_ptr;
+    }
+#endif /* RTC_USE_CUDA */
     };
 _RTC_END
 
